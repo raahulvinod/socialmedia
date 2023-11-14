3e77ab85c6fb84422815ea766996223d113a8cc3
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-14 14:43:27 +0530

    feat: add react-router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from 'pages/homePage/Home';
+import Login from 'pages/loginPage/Login';
+import Profile from 'pages/profilePage/Profile';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
