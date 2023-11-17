371034dc273b1dbc3df96b09cd6f9fec83f6e75a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-17 11:42:23 +0530

    feat: add widgetwrappers

---
+++ client/src/components/widgets/UserWidget.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined,
+} from '@mui/icons-material';
+import { Box, Typography, Divider, useTheme } from '@mui/material';
+
+import UserImage from '../UserImage';
+import FlexBetween from '../FlexBetween';
+import WidgetWrapper from '../WidgetWrapper';
+
+const UserWidget = () => {
+  return;
+  <div>UserWidget</div>;
+};
+
+export default UserWidget;
