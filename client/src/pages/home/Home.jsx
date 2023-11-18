3850bd6850cbef0cc13c5c0cb10e38ee5c999d1a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-18 12:58:10 +0530

    feat: add home page and widgets

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material';
 import Navbar from 'pages/navbar/Navbar';
+import UserWidget from 'components/widgets/UserWidget';
 
 const Home = () => {
   return (
     <Box>
       <Navbar />
+      <UserWidget />
     </Box>
   );
 };
